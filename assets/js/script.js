a196630b695d4964478467ffd19daabe899015f3
Marcel <marcelpodsiadlo.business@gmail.com>
2023-10-20 22:04:18 +0200

    Update index.html and main.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,59 +37,35 @@ function togglePatriotsInformation() {
   }
 }
 
-// Patriots 
-const patriotAdamMickiewiczButton = document.getElementById('adam_mickiewicz').addEventListener('click', () =>{
-  togglePatriots(divAM)
-})
-const patriotJuliuszSlowackiButton = document.getElementById('juliusz_slowacki').addEventListener('click', () =>{
-  togglePatriots(divJS)
-})
-const patriotJozefBemButton = document.getElementById('jozef_bem').addEventListener('click', () =>{
-  togglePatriots(divJB)
-})
-const patriotAdamJCzarnoryskiButton = document.getElementById('adam_jerzy_czartoryski').addEventListener('click', () =>{
-  togglePatriots(divAJC)
-})
-const patriotBonawenturaNiemojowskiButton = document.getElementById('bonawentura_niemojowski').addEventListener('click', () =>{
-  togglePatriots(divBM)
-})
-const patriotTeodorMorawskiButton = document.getElementById('teodor_morawski').addEventListener('click', () =>{
-  togglePatriots(divTM)
-})
-const patriotErnestMalinowskiButton = document.getElementById('ernest_malinowski').addEventListener('click', () =>{
-  togglePatriots(divEM)
-})
-const patriotZygmuntKrasinskiButton = document.getElementById('zygmunt_krasinski').addEventListener('click', () =>{
-  togglePatriots(divZK)
-})
-const patriotFryderykChopinButton = document.getElementById('fryderyk_chopin').addEventListener('click', () =>{
-  togglePatriots(divFC)
-})
-const patriotMaurycyMochniackiButton = document.getElementById('maurycy_mochnacki').addEventListener('click', () =>{
-  togglePatriots(divMM)
-})
-// Patriots div
-const divAM = document.getElementById('patriots_board_am')
-const divJS = document.getElementById('patriots_board_js')
-const divJB = document.getElementById('patriots_board_jb')
-const divAJC = document.getElementById('patriots_board_ajc')
-const divBN = document.getElementById('patriots_board_gn')
-const divTM = document.getElementById('patriots_board_tm')
-const divEM = document.getElementById('patriots_board_em')
-const divZK = document.getElementById('patriots_board_zk')
-const divFC = document.getElementById('patriots_board_fc')
-const divMM = document.getElementById('patriots_board_mm')
+const patriotButtons = document.querySelectorAll('.patriots');
+const allDivs = document.querySelectorAll('.patriots_board_class');
 
-function togglePatriots(div){
-  if(div.style.display == 'none' || div.style.display == ''){
-    div.style.display = 'block'
-    div.style.zIndex = '1'
-    document.addEventListener('click', () => {
-      
-    })
-  }
-  else if (div.style.display == 'block'){
-    div.style.display = 'none'
-    div.style.zIndex = '0'
+function hideAllPatriotDivs() {
+  allDivs.forEach((div) => {
+    div.style.display = 'none';
+  });
+}
+
+// Funkcja do obsługi kliknięcia przycisku patriota
+function togglePatriot(patriotDiv) {
+  if (patriotDiv.style.display === 'block') {
+    patriotDiv.style.display = 'none';
+  } else {
+    hideAllPatriotDivs();
+    patriotDiv.style.display = 'block';
   }
 }
+
+patriotButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const patriotDiv = allDivs[index];
+    togglePatriot(patriotDiv);
+  });
+});
+
+// Dodaj event listener do okna, aby zamykać divy po kliknięciu na dowolnym obszarze strony
+window.addEventListener('click', (event) => {
+  if (!event.target.classList.contains('patriots')) {
+    hideAllPatriotDivs();
+  }
+});
